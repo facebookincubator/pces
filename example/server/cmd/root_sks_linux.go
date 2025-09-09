67c2c3eab18c7898e8ffeb68e291aebc21f066c2
Ekaterina Braun <ebraun@meta.com>
2025-09-09 07:42:59 -0700

    SKS Linux integration
    Summary: Integration of SKS library to PCeS based on the: https://www.internalfb.com/diff/D81233721
    
    Reviewed By: n-canter
    
    Differential Revision: D81723896
    
    fbshipit-source-id: b52ae3e65f6eb1fd69ecd2d20f6b7465c70ab329

--- example/server/cmd/root_sks_linux.go
+++ example/server/cmd/root_sks_linux.go
@@ -17,16 +17,94 @@
 package cmd
 
 import (
+	"bytes"
 	"crypto"
-	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+
+	"github.com/facebookincubator/sks"
 )
+
+var sksKeyDir string
+
+func init() {
+	rootCmd.Flags().StringVar(&sksKeyDir, "sks-key-dir", "", "Directory to store SKS key files persistently.")
+
+	rootCmd.MarkFlagRequired("sks-key-dir")
+}
 
 // generateSSHCertPrivateKey generates a certificate private key using SKS for SSH
 func generateSSHCertPrivateKey() (crypto.Signer, error) {
-	return nil, fmt.Errorf("not implemented")
+	sksKeyPath := filepath.Join(sksKeyDir, "pces-sks-key.blob.ssh")
+	akPath := filepath.Join(sksKeyDir, "pces-ak-key.blob.ssh")
+	return createSKSKey("ssh-cert", "pces-ssh-cert", sksKeyPath, akPath)
 }
 
 // generateTLSCertPrivateKey generates a certificate private key using SKS for TLS
 func generateTLSCertPrivateKey() (crypto.Signer, error) {
-	return nil, fmt.Errorf("not implemented")
+	sksKeyPath := filepath.Join(sksKeyDir, "pces-sks-key.blob.tls")
+	akPath := filepath.Join(sksKeyDir, "pces-ak-key.blob.tls")
+	return createSKSKey("tls-cert", "pces-tls-cert", sksKeyPath, akPath)
+}
+
+// createSKSKey creates an SKS key by loading existing key blobs from disk if available,
+// creating the key with SKS, and storing updated blobs back to disk if they differ
+func createSKSKey(keyLabel, keyTag, sksKeyPath, akPath string) (sks.Key, error) {
+	sksKeyBlob, akBlob, err := loadKeys(sksKeyPath, akPath)
+	if err != nil {
+		return nil, err
+	}
+
+	key, err := sks.New(keyLabel, keyTag, sks.WithAKKeyBlob(akBlob), sks.WithSKSKeyBlob(sksKeyBlob))
+	if err != nil {
+		return nil, err
+	}
+
+	newAKBlob, newSKSBlob, err := key.EncryptedBlob()
+	if err != nil {
+		return nil, err
+	}
+
+	if !bytes.Equal(newSKSBlob, sksKeyBlob) || !bytes.Equal(newAKBlob, akBlob) {
+		slog.Warn("Key blobs have changed, updating stored keys for %s", keyLabel)
+		if err := storeKeys(newSKSBlob, sksKeyPath, newAKBlob, akPath); err != nil {
+			return nil, err
+		}
+	}
+
+	return key, nil
+}
+
+func loadKeys(sksKeyPath string, akPath string) (keyBlob []byte, akBlob []byte, err error) {
+	keyBlob, err = os.ReadFile(sksKeyPath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	akBlob, err = os.ReadFile(akPath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return
+}
+
+func storeKeys(sksKeyBlob []byte, sksKeyPath string, akBlob []byte, akPath string) error {
+	if err := os.MkdirAll(filepath.Dir(sksKeyPath), 0700); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(filepath.Dir(akPath), 0700); err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(akPath, akBlob, 0600); err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(sksKeyPath, sksKeyBlob, 0600); err != nil {
+		return err
+	}
+
+	return nil
 }
